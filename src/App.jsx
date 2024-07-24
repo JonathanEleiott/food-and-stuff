5cd57f0b425dbe7f1c386ae83db475735ffac59d
Jonathan Eleiott <jonathan.eleiott@fullstackacademy.com>
2024-07-24 12:24:02 -0400

    add details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import AllProducts from './AllProducts.jsx';
+import SingleProduct from './SingleProduct.jsx'
 
 const App = () => {
 
   return (
     <>
+      
+
+
       <Routes>
         <Route path='/' element={<h1>Welcome to Food and Stuff</h1>} />
         <Route path='/products' element={<AllProducts />} />
+        <Route path='/products/:id' element={<SingleProduct />} />
       </Routes>
     </>
   )
